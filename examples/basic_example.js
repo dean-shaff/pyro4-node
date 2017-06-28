8bdaf6c3d662267b07aa2cc5d59b211d7e65373e
Dean Shaff <dshaff001@gmail.com>
2017-06-28 12:20:10 +0400

    Added own implementation of sizeOf

--- examples/basic_example.js
+++ examples/basic_example.js
@@ -3,26 +3,18 @@ const Promise = require("promise")
 const remote = require("./../lib/proxy");
 const logging = require('./../lib/logging');
 
-logging.setLevel("INFO");
+logging.setLevel("DEBUG");
 
 dataCallback = function(msg){
     console.log("main: Message data: {}".format(msg.data))
 }
 
-// function method1(){
-//     var p = new remote.Proxy('localhost', 50001, 'BasicServer');
-//     p.init(() => {
-//         return [p.callMethod('square',[2], null, dataCallback),
-//                 p.callMethod('cube', [3], null, dataCallback)]
-//     });
-// }
-
-function method2(){
+function main(){
     var p = new remote.Proxy('localhost', 50001, 'BasicServer');
     var res = p.callMethod('square', [2], dataCallback);
     res.getResult();
     var res1 = p.callMethod('cube', [100], dataCallback);
     res1.getResult();
 }
-// method1()
-method2()
+
+main();
