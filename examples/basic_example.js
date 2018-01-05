0d67af0e20ba7c04da62b7b07ffc119e92ac5818
Dean Shaff <dshaff001@gmail.com>
2018-01-05 21:04:31 +0400

    Implemented means of calling methods recursively. Going to try batched calls next

--- examples/basic_example.js
+++ examples/basic_example.js
@@ -1,6 +1,6 @@
 const remote = require("./../lib/proxy");
 
-var dataHandler = (data)=>{
+var dataHandler = (data, proxy)=>{
     console.log(data)
 }
 
@@ -8,10 +8,39 @@ var proxyHandler = (proxy)=>{
     proxy.square({args:[2]}, dataHandler)
 }
 
-var main = function(){
+var proxyHandler = (proxy)=>{
+    var iter = 0
+    dataHandler = (data, proxy)=>{
+        console.log(data)
+        if (iter < 100){
+            proxy.cube({args:[iter]}, dataHandler)
+        }
+        iter += 1
+    }
+    proxy.square({args:[2]},dataHandler)
+}
+
+var errorHandler = (error)=>{
+    console.error(error.msg)
+}
+
+/**
+ * Example using the "locateNS" funnction. This allows
+ * you to find any object registered on the nameserver
+ */
+var main_locateNS = function(){
     remote.locateNS(["localhost", 9090], (ns)=>{
         ns.lookup("BasicServer", proxyHandler)
     })
 }
 
-main()
+/**
+ * Example using the address and object name of the server.
+ */
+var main_directAddress = function(){
+    var proxy = new remote.Proxy({port:50001, host:"localhost", objName:"BasicServer"})
+    proxy.init(proxyHandler, errorHandler)
+}
+
+// main_locateNS()
+main_directAddress()
